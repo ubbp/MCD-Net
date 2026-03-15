b95d21bc0e6fdf5d2bbe25036bb656e52062f873
ubbp <48211482+ubbp@users.noreply.github.com>
2026-03-15 14:50:40 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ CUDA_VISIBLE_DEVICES=0 python test.py \
     --split test \
     --window12 \
     --img_size 480 \
-    --num_tmem 1 \
+    --num_adf 1 \
     --resume model_best_refsegrs.pth \
     --save_dir ./your_save_dir
