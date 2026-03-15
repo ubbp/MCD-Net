68de09ad9481491446808a961cf1f7770e6b1191
ubbp <48211482+ubbp@users.noreply.github.com>
2026-03-15 14:51:01 +0800

    Change num_tmem to num_adf in training script

--- train.sh
+++ train.sh
@@ -3,6 +3,6 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
     --model_id MCDNet  \
     --epochs 100  \
     --lr 5e-5   \
-    --num_tmem 1 \
+    --num_adf 1 \
     --output-dir ./your_output_path \
     --refer_data_root ./your_data_path  
